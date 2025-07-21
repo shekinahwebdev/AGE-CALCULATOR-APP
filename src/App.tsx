0583a6680db9bd05ed41b99c66b68a8f2e6bf87c
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-21 19:36:03 +0000

    Add README template, style guide, and implement InputData component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { InputData } from "./components/InputData";
 
 function App() {
-  return <main className="main_calculator"></main>;
+  return (
+    <main className="main_calculator">
+      <InputData />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/InputData.tsx
@@ -0,0 +1,9 @@
+export const InputData = () => {
+  return (
+    <section className="input_data">
+      <div className="input_data__container">
+        <p className="data_day">Day</p>
+      </div>
+    </section>
+  );
+};
