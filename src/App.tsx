8a4d6866c7a22dbbd116cf6b5f1a772036a57d4a
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-21 19:45:15 +0000

    Add input fields for day, month, and year; enhance App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import "./App.css";
 import { InputData } from "./components/InputData";
+import arrowIcon from "/assets/images/icon-arrow.svg";
 
 function App() {
   return (
     <main className="main_calculator">
       <InputData />
+      <div className="arrow_line">
+        <div className="line"></div>
+        <div className="arrow">
+          <img src={arrowIcon} alt="Arrow Icon" />
+        </div>
+      </div>
+      <section className="output_container">
+        <div className="output_data years">
+          <p className="output_year">--</p>
+          <p className="output_label">Years</p>
+        </div>
+        <div className="output_data months">
+          <p className="output_year">--</p>
+          <p className="output_label">month</p>
+        </div>
+        <div className="output_data days">
+          <p className="output_year">--</p>
+          <p className="output_label">days</p>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/InputData.tsx
+++ src/components/InputData.tsx
@@ -3,6 +3,33 @@ export const InputData = () => {
     <section className="input_data">
       <div className="input_data__container">
         <p className="data_day">Day</p>
+        <input
+          type="number"
+          className="input_data__day"
+          placeholder="DD"
+          maxLength={2}
+          min={1}
+        />
+      </div>
+      <div className="input_data__container">
+        <p className="data_month">Month</p>
+        <input
+          type="number"
+          className="input_data__month"
+          placeholder="MM"
+          maxLength={2}
+          min={1}
+        />
+      </div>
+      <div className="input_data__container">
+        <p className="data_year">Year</p>
+        <input
+          type="number"
+          className="input_data__year"
+          placeholder="YYYY"
+          maxLength={4}
+          min={1900}
+        />
       </div>
     </section>
   );
