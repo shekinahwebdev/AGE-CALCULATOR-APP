34b707a816aa239037ad2b0e58fd721dd658db21
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-22 23:58:43 +0000

    Add Formik for form handling in InputData component and implement validation

--- src/components/InputData.tsx
+++ src/components/InputData.tsx
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 interface InputDataProps {
   handleDayChange: (value: React.ChangeEvent<HTMLInputElement>) => void;
   handleMonthChange: (value: React.ChangeEvent<HTMLInputElement>) => void;
@@ -15,6 +16,44 @@ export const InputData: React.FC<InputDataProps> = ({
   monthValue,
   yearValue,
 }) => {
+  const formik = useFormik({
+    initialValues: {
+      day: "",
+      month: "",
+      year: "",
+    },
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.day.trim()) {
+        errors.day = "This field is required";
+      }
+
+      if (!values.month.trim()) {
+        errors.month = "This field is required";
+      }
+
+      if (!values.year.trim()) {
+        errors.year = "This field is required";
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      alert("Form submitted successfully");
+      console.log(values);
+      // page reload
+      window.location.reload();
+    },
+  });
+
+  // input fields
+  const fields = [
+    { name: "day", placeholder: "DD" },
+    { name: "month", placeholder: "MM" },
+    { name: "year", placeholder: "YYYY" },
+  ];
+
   return (
     <section className="input_data">
       <div className="input_data__container">
@@ -22,7 +61,7 @@ export const InputData: React.FC<InputDataProps> = ({
         <input
           type="number"
           className="data input_data__day"
-          placeholder="DD"
+          placeholder={fields[0].placeholder}
           value={dayValue}
           maxLength={2}
           onChange={handleDayChange}
@@ -34,7 +73,7 @@ export const InputData: React.FC<InputDataProps> = ({
         <input
           type="number"
           className="data input_data__month"
-          placeholder="MM"
+          placeholder={fields[1].placeholder}
           value={monthValue}
           maxLength={2}
           onChange={handleMonthChange}
@@ -46,7 +85,7 @@ export const InputData: React.FC<InputDataProps> = ({
         <input
           type="number"
           className="data input_data__year"
-          placeholder="YYYY"
+          placeholder={fields[2].placeholder}
           value={yearValue}
           maxLength={4}
           onChange={handleYearChange}
